46d566edc33a7133227e29f4ae6e3a749c1194d5
Konrad Hapke <hap19520@spengergasse.at>
2022-05-08 19:41:26 +0200

    initial

---
+++ src/App.svelte
@@ -0,0 +1,203 @@
+<script script lang="ts">
+	import { onMount } from "svelte";
+	import shader from "./shaders/mandelbrot.wgsl";
+
+	let canvas: HTMLCanvasElement;
+	
+	async function init() {
+		function exit() {
+			console.error("No GPU adapter found! Try enabling the experimental flag \"#enable-unsafe-webgpu\" under \"chrome://flags\"");
+			return;
+		}
+		
+		if (!navigator.gpu) exit();
+		const adapter = await navigator.gpu.requestAdapter();
+		if(!adapter)
+			exit();
+  		const device = await adapter.requestDevice();
+		const context = canvas.getContext('webgpu');
+
+		const pixelRatio = window.devicePixelRatio || 1;
+		const presentationSize = [
+			canvas.clientWidth * pixelRatio,
+			canvas.clientHeight * pixelRatio
+		];
+		const presentationFormat = context.getPreferredFormat(adapter);
+
+		context.configure({
+			device,
+			format: presentationFormat,
+			size: presentationSize
+		});
+
+		const shaderModule = device.createShaderModule({
+			code: shader,
+		});
+
+		const pipeline = device.createRenderPipeline({
+			vertex: {
+				module: shaderModule,
+				entryPoint: 'vs_main',
+			},
+			fragment: {
+				module: shaderModule,
+				entryPoint: 'fs_main',
+				targets: [
+					{
+						format: presentationFormat,
+					}
+				]
+			},
+			primitive: {
+				topology: 'triangle-strip',
+			}
+		});
+
+		class InputData {
+			zoom: number;
+			x: number;
+			y: number;
+			r_mod: number;
+			g_mod: number;
+			b_mod: number;
+
+			constructor() {
+				this.zoom = 0;
+				this.x = 0;
+				this.y = 0;
+				this.r_mod = 1;
+				this.g_mod = 2;
+				this.b_mod = 3;
+			}
+		}
+		
+		const inputBufferSize = 6 * 4;
+		const inputBuffer = device.createBuffer({
+			size: inputBufferSize,
+			usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+		});
+
+		const inputBindGroup = device.createBindGroup({
+			layout: pipeline.getBindGroupLayout(0),
+			entries: [
+				{
+					binding: 0,
+					resource: {
+						buffer: inputBuffer,
+					}
+				}
+			]
+		});
+
+		let inputData = new InputData();
+		
+		var pressedKeys: string[] = [];
+
+		function frame() {
+			if(!context) return;
+
+			document.addEventListener("keydown", (e) => {
+				if(!pressedKeys.includes(e.key))
+				pressedKeys.push(e.key.toLowerCase());
+			});
+			document.addEventListener("keyup", (e) => {
+				pressedKeys.splice(pressedKeys.indexOf(e.key), 1);
+			});
+
+			const speed = .01;
+			const zoomSpeed = speed;
+			const moveSpeed = speed / Math.pow(Math.E, inputData.zoom);
+
+			if(pressedKeys.includes("w")) {
+				inputData.y += moveSpeed;
+			}
+			if(pressedKeys.includes("s")) {
+				inputData.y -= moveSpeed;
+			}
+			if(pressedKeys.includes("d")) {
+				inputData.x += moveSpeed;
+			}
+			if(pressedKeys.includes("a")) {
+				inputData.x -= moveSpeed;
+			}
+			if(pressedKeys.includes("+")) {
+				inputData.zoom += zoomSpeed;
+			}
+			if(pressedKeys.includes("-")) {
+				inputData.zoom -= zoomSpeed;
+			}
+			if(pressedKeys.includes("1")) {
+				inputData.r_mod += zoomSpeed;
+			}
+			if(pressedKeys.includes("2")) {
+				inputData.r_mod -= zoomSpeed;
+			}
+			if(pressedKeys.includes("3")) {
+				inputData.g_mod += zoomSpeed;
+			}
+			if(pressedKeys.includes("4")) {
+				inputData.g_mod -= zoomSpeed;
+			}
+			if(pressedKeys.includes("5")) {
+				inputData.b_mod += zoomSpeed;
+			}
+			if(pressedKeys.includes("6")) {
+				inputData.b_mod -= zoomSpeed;
+			}
+			if(pressedKeys.includes("r")) {
+				inputData = new InputData();
+			}
+			// randomize colors 
+			if(pressedKeys.includes("c")) {
+				inputData.r_mod = Math.random();
+				inputData.b_mod = Math.random();
+				inputData.g_mod = Math.random();
+			}
+			
+			const commandEncoder = device.createCommandEncoder();
+
+			device.queue.writeBuffer(inputBuffer, 0, new Float32Array(Object.entries(inputData).map(([key, value]) => value)));
+			
+			const textureView = context.getCurrentTexture().createView();
+
+			const renderPassDescriptor: GPURenderPassDescriptor = {
+				colorAttachments: [
+					{
+						view: textureView,
+						clearValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
+						loadOp: 'clear',
+						storeOp: 'store',
+					},
+				],
+			};
+
+			const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+			passEncoder.setPipeline(pipeline);
+			passEncoder.setBindGroup(0, inputBindGroup);
+			passEncoder.draw(4);
+			passEncoder.end();
+
+			device.queue.submit([commandEncoder.finish()]);
+			requestAnimationFrame(frame);
+		}
+
+		requestAnimationFrame(frame);
+	}
+
+	
+
+	onMount(() => {
+		init();
+	});
+
+	init();
+</script>
+
+<canvas bind:this="{canvas}"></canvas>
+
+<style>
+	canvas {
+		width: 100%;
+		height: 100%;
+	}
+</style>
---
+++ src/shaders/mandelbrot.wgsl
@@ -0,0 +1,67 @@
+// Vertex shader
+
+struct VertexOutput {
+    @builtin(position) clip_position: vec4<f32>;
+    @location(0) position: vec2<f32>;
+};
+
+struct InputData {
+    zoom: f32;
+    x: f32;
+    y: f32;
+    r_mod: f32;
+    g_mod: f32;
+    b_mod: f32;
+};
+
+@group(0) @binding(0)
+var<uniform> input_data: InputData;
+
+
+@stage(vertex)
+fn vs_main(
+    @builtin(vertex_index) in_vertex_index: u32,
+) -> VertexOutput {
+    var positions = array<vec2<f32>, 4>(
+        vec2<f32>(-1.0, -1.0),
+        vec2<f32>(1.0, -1.0),
+        vec2<f32>(-1.0, 1.0),
+        vec2<f32>(1.0, 1.0),
+    );
+    
+    let e = 2.7182;
+    var out: VertexOutput;
+    let x = f32(1 - i32(in_vertex_index)) * 0.05;
+    let y = f32(i32(in_vertex_index & 1u) * 2 - 1) * 0.05;
+    out.clip_position = vec4<f32>(positions[in_vertex_index].xy, 0.0, 1.0);
+    out.position = vec2<f32>(positions[in_vertex_index].x * 2.0 - 0.5, positions[in_vertex_index].y);
+    out.position = (out.position) * pow(e, -input_data.zoom)  + vec2<f32>(input_data.x, input_data.y);
+    return out;
+}
+
+// Fragment shader
+
+fn square_complex(c: vec2<f32>) -> vec2<f32> {
+    let x = c.x * c.x - c.y * c.y;
+    let y = 2.0 * c.x * c.y;
+    return vec2<f32>(x, y);
+}
+
+@stage(fragment)
+fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
+    let iterations = 1000;
+
+    var z = vec2<f32>(0.0);
+    let c = in.position;
+
+    for(var i = 0; i < iterations; i = i + 1) {
+        z = square_complex(z) + c;
+        if(length(z) > 4.0) {
+            let f = f32(i) / 10.0;
+            return vec4<f32>(f % input_data.r_mod, f % input_data.g_mod, f % input_data.b_mod, 1.0);
+        }
+    }
+
+    return vec4<f32>(0.0, 0.0, 0.0, 0.0);
+}
+ 
