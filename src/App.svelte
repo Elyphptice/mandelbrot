fadb9fa545d825d02683474f13e218a0d00c15f0
Konrad Hapke <hap19520@spengergasse.at>
2023-08-25 14:36:40 +0200

    webgpu changes

--- src/App.svelte
+++ src/App.svelte
@@ -4,31 +4,32 @@
 	import { mat3, mat4, vec2, vec3 } from "gl-matrix";
 
 	let canvas: HTMLCanvasElement;
-	
+
 	async function init() {
 		function exit() {
-			console.error("No GPU adapter found! Try enabling the experimental flag \"#enable-unsafe-webgpu\" under \"chrome://flags\"");
+			console.error(
+				'No GPU adapter found! Try enabling the experimental flag "#enable-unsafe-webgpu" under "chrome://flags"'
+			);
 			return;
 		}
-		
+
 		if (!navigator.gpu) exit();
 		const adapter = await navigator.gpu.requestAdapter();
-		if(!adapter)
-			exit();
-  		const device = await adapter.requestDevice();
-		const context = canvas.getContext('webgpu');
+		if (!adapter) exit();
+		const device = await adapter.requestDevice();
+		const context = canvas.getContext("webgpu");
 
 		const pixelRatio = window.devicePixelRatio || 1;
 		const presentationSize = [
 			canvas.clientWidth * pixelRatio,
-			canvas.clientHeight * pixelRatio
+			canvas.clientHeight * pixelRatio,
 		];
-		const presentationFormat = context.getPreferredFormat(adapter);
+		const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
 
 		context.configure({
 			device,
 			format: presentationFormat,
-			size: presentationSize
+			size: presentationSize,
 		});
 
 		const shaderModule = device.createShaderModule({
@@ -36,22 +37,24 @@
 		});
 
 		const pipeline = device.createRenderPipeline({
+			// @ts-ignore
+			layout: "auto",
 			vertex: {
 				module: shaderModule,
-				entryPoint: 'vs_main',
+				entryPoint: "vs_main",
 			},
 			fragment: {
 				module: shaderModule,
-				entryPoint: 'fs_main',
+				entryPoint: "fs_main",
 				targets: [
 					{
 						format: presentationFormat,
-					}
-				]
+					},
+				],
 			},
 			primitive: {
-				topology: 'triangle-strip',
-			}
+				topology: "triangle-strip",
+			},
 		});
 
 		class InputData {
@@ -74,39 +77,55 @@
 				camera.far = 100;
 
 				this.mouse = vec2.create();
-				
+
 				this.projection = createPerpective(camera);
 			}
 		}
 
 		function createPerpective(camera: Camera) {
-			var projection = new Float32Array(16);	
-			let top = Math.tan(camera.fieldOfView * Math.PI / 360);
+			var projection = new Float32Array(16);
+			let top = Math.tan((camera.fieldOfView * Math.PI) / 360);
 			let right = camera.aspectRatio * top;
-			
+
 			projection[0] = 1.0 / right;
 			projection[5] = 1.0 / top;
 			projection[10] = camera.far / (camera.far - camera.near);
-			projection[14] = -(camera.far * camera.near) / (camera.far - camera.near);
+			projection[14] =
+				-(camera.far * camera.near) / (camera.far - camera.near);
 			projection[11] = 1.0;
 			return projection;
 		}
 
 		function multiply(point: Float32Array, mat: Float32Array) {
 			var result = new Float32Array(4);
-			result[0] = point[0] * mat[0] + point[1] * mat[1] + point[2] * mat[2] + point[3] * mat[3];
-			result[1] = point[0] * mat[4] + point[1] * mat[5] + point[2] * mat[6] + point[3] * mat[7];
-			result[2] = point[0] * mat[8] + point[1] * mat[9] + point[2] * mat[10] + point[3] * mat[11];
-			result[3] = point[0] * mat[12] + point[1] * mat[13] + point[2] * mat[14] + point[3] * mat[15];
+			result[0] =
+				point[0] * mat[0] +
+				point[1] * mat[1] +
+				point[2] * mat[2] +
+				point[3] * mat[3];
+			result[1] =
+				point[0] * mat[4] +
+				point[1] * mat[5] +
+				point[2] * mat[6] +
+				point[3] * mat[7];
+			result[2] =
+				point[0] * mat[8] +
+				point[1] * mat[9] +
+				point[2] * mat[10] +
+				point[3] * mat[11];
+			result[3] =
+				point[0] * mat[12] +
+				point[1] * mat[13] +
+				point[2] * mat[14] +
+				point[3] * mat[15];
 			return result;
 		}
-		
+
 		const inputBufferSize = 208;
 		const inputBuffer = device.createBuffer({
 			size: inputBufferSize,
 			usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
 		});
-
 
 		const inputBindGroup = device.createBindGroup({
 			layout: pipeline.getBindGroupLayout(0),
@@ -115,46 +134,40 @@
 					binding: 0,
 					resource: {
 						buffer: inputBuffer,
-					}
-				}
-			]
+					},
+				},
+			],
 		});
 
 		let inputData = new InputData();
 
-		
-		
-		var pressedKeys: { [id: string] : boolean } = {};
+		var pressedKeys: { [id: string]: boolean } = {};
 
 		var startTime = performance.now();
 
-
 		// onclick
 		canvas.addEventListener("click", (e) => {
-			var havePointerLock = 'pointerLockElement' in document ||
-			'mozPointerLockElement' in document ||
-			'webkitPointerLockElement' in document;
-			
-			canvas.requestPointerLock = canvas.requestPointerLock 
-			// @ts-ignore
-			|| canvas.webkitRequestPointerLock || canvas.mozRequestPointerLock;
-		
+			var havePointerLock =
+				"pointerLockElement" in document ||
+				"mozPointerLockElement" in document ||
+				"webkitPointerLockElement" in document;
+
+			canvas.requestPointerLock =
+				canvas.requestPointerLock ||
+				// @ts-ignore
+				canvas.webkitRequestPointerLock ||
+				canvas.mozRequestPointerLock;
+
 			canvas.requestPointerLock();
 		});
 
 		document.addEventListener("mousemove", moveCallback, false);
 
-
-		
 		function moveCallback(e) {
-			var movementX = e.movementX ||
-				e.mozMovementX          ||
-				e.webkitMovementX       ||
-				0,
-			movementY = e.movementY ||
-				e.mozMovementY      ||
-				e.webkitMovementY   ||
-				0;
+			var movementX =
+					e.movementX || e.mozMovementX || e.webkitMovementX || 0,
+				movementY =
+					e.movementY || e.mozMovementY || e.webkitMovementY || 0;
 
 			inputData.mouse[0] += movementX;
 			inputData.mouse[1] += movementY;
@@ -166,51 +179,59 @@
 		document.addEventListener("keyup", (e) => {
 			pressedKeys[e.key] = false;
 		});
-		
-		function frame() {
-			if(!context) return;
 
-			
+		function frame() {
+			if (!context) return;
+
 			//#region input
 
-			
-
-			const speed = .01;
+			const speed = 0.01;
 
 			let input = vec3.create();
-			
-			if(pressedKeys["w"]) {
+
+			if (pressedKeys["w"]) {
 				input[2] -= speed;
 			}
-			if(pressedKeys["s"]) {
+			if (pressedKeys["s"]) {
 				input[2] += speed;
 			}
-			if(pressedKeys["d"]) {
+			if (pressedKeys["d"]) {
 				input[0] -= speed;
 			}
-			if(pressedKeys["a"]) {
+			if (pressedKeys["a"]) {
 				input[0] += speed;
 			}
-			if(pressedKeys["e"]) {
+			if (pressedKeys["e"]) {
 				input[1] -= speed;
 			}
-			if(pressedKeys["q"]) {
+			if (pressedKeys["q"]) {
 				input[1] += speed;
 			}
-			if(pressedKeys["r"]) {
+			if (pressedKeys["r"]) {
 				// inputData = new InputData();
 			}
 
-			
 			//#endregion
-			
+
 			const commandEncoder = device.createCommandEncoder();
-			
+
 			var projection = mat4.fromValues(
-				inputData.projection[0], inputData.projection[1], inputData.projection[2], inputData.projection[3],
-				inputData.projection[4], inputData.projection[5], inputData.projection[6], inputData.projection[7],
-				inputData.projection[8], inputData.projection[9], inputData.projection[10], inputData.projection[11],
-				inputData.projection[12], inputData.projection[13], inputData.projection[14], inputData.projection[15]
+				inputData.projection[0],
+				inputData.projection[1],
+				inputData.projection[2],
+				inputData.projection[3],
+				inputData.projection[4],
+				inputData.projection[5],
+				inputData.projection[6],
+				inputData.projection[7],
+				inputData.projection[8],
+				inputData.projection[9],
+				inputData.projection[10],
+				inputData.projection[11],
+				inputData.projection[12],
+				inputData.projection[13],
+				inputData.projection[14],
+				inputData.projection[15]
 			);
 
 			var view = mat4.create();
@@ -222,31 +243,45 @@
 
 			let y = input[1];
 			input[1] = 0;
-			
+
 			vec3.transformMat4(input, input, view);
-			
+
 			inputData.x += input[0];
 			inputData.y += y;
 			inputData.z += input[2];
-			
+
 			// mat4.translate(view, view, [inputData.x, inputData.y, inputData.z]);
 
 			let inverseProjection = mat4.create();
 			mat4.invert(inverseProjection, projection);
-			
+
 			mat4.multiply(projection, projection, view);
-			
-			
+
 			device.queue.writeBuffer(inputBuffer, 0, new Float32Array(view));
 
 			var currentTime = performance.now();
 			currentTime = (currentTime - startTime) / 1000;
-			
-			device.queue.writeBuffer(inputBuffer, 64, new Float32Array(inverseProjection));
-			device.queue.writeBuffer(inputBuffer, 128, new Float32Array(inverseView));
-			device.queue.writeBuffer(inputBuffer, 192, new Float32Array([inputData.x, inputData.y, inputData.z]));
-			device.queue.writeBuffer(inputBuffer, 204, new Float32Array([currentTime]));
 
+			device.queue.writeBuffer(
+				inputBuffer,
+				64,
+				new Float32Array(inverseProjection)
+			);
+			device.queue.writeBuffer(
+				inputBuffer,
+				128,
+				new Float32Array(inverseView)
+			);
+			device.queue.writeBuffer(
+				inputBuffer,
+				192,
+				new Float32Array([inputData.x, inputData.y, inputData.z])
+			);
+			device.queue.writeBuffer(
+				inputBuffer,
+				204,
+				new Float32Array([currentTime])
+			);
 
 			const textureView = context.getCurrentTexture().createView();
 
@@ -255,13 +290,14 @@
 					{
 						view: textureView,
 						clearValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
-						loadOp: 'clear',
-						storeOp: 'store',
+						loadOp: "clear",
+						storeOp: "store",
 					},
 				],
 			};
 
-			const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+			const passEncoder =
+				commandEncoder.beginRenderPass(renderPassDescriptor);
 			passEncoder.setPipeline(pipeline);
 			passEncoder.setBindGroup(0, inputBindGroup);
 			passEncoder.draw(4);
@@ -281,21 +317,37 @@
 		public far: number;
 	}
 
-	function getFrustumCorners(camera: Camera): mat4{
+	function getFrustumCorners(camera: Camera): mat4 {
 		const fov = camera.fieldOfView;
 		const aspect = camera.aspectRatio;
 
-		const fovTan = Math.tan(fov * 0.5 * Math.PI / 180);
+		const fovTan = Math.tan((fov * 0.5 * Math.PI) / 180);
 
 		let frustomCorners = mat4.create();
 
 		const toRight = vec3.fromValues(fovTan * aspect, 0, 0);
 		const toTop = vec3.fromValues(0, fovTan * aspect, 0);
 
-		frustomCorners = setRow(frustomCorners, 0, add(backward, neg(toRight), toTop)); // top-left
-		frustomCorners = setRow(frustomCorners, 1, add(backward, toRight, toTop)); // top-right
-		frustomCorners = setRow(frustomCorners, 2, add(backward, toRight, neg(toTop))); // bottom-right
-		frustomCorners = setRow(frustomCorners, 3, add(backward, neg(toRight), neg(toTop))); // bottom-left
+		frustomCorners = setRow(
+			frustomCorners,
+			0,
+			add(backward, neg(toRight), toTop)
+		); // top-left
+		frustomCorners = setRow(
+			frustomCorners,
+			1,
+			add(backward, toRight, toTop)
+		); // top-right
+		frustomCorners = setRow(
+			frustomCorners,
+			2,
+			add(backward, toRight, neg(toTop))
+		); // bottom-right
+		frustomCorners = setRow(
+			frustomCorners,
+			3,
+			add(backward, neg(toRight), neg(toTop))
+		); // bottom-left
 
 		return frustomCorners;
 	}
@@ -320,8 +372,6 @@
 		return vec;
 	}
 
-	
-	
 	onMount(() => {
 		init();
 	});
@@ -329,7 +379,8 @@
 	init();
 </script>
 
-<canvas bind:this="{canvas}"></canvas>
+<canvas bind:this={canvas} />
+
 <style>
 	canvas {
 		width: 100%;
--- src/shaders/raymarching.wgsl
+++ src/shaders/raymarching.wgsl
@@ -1,18 +1,18 @@
 // Vertex shader
 
 struct VertexOutput {
-    @builtin(position) clip_position: vec4<f32>;
-    @location(0) position: vec3<f32>;
-    @location(1) ray: vec3<f32>;
-    @location(2) screen_coordinates: vec2<f32>;
+    @builtin(position) clip_position: vec4<f32>,
+    @location(0) position: vec3<f32>,
+    @location(1) ray: vec3<f32>,
+    @location(2) screen_coordinates: vec2<f32>,
 };
 
 struct InputData {
-    projection: mat4x4<f32>;
-    inverse_projection: mat4x4<f32>;
-    inverse_view: mat4x4<f32>;
-    camera_position: vec3<f32>;
-    time: f32;
+    projection: mat4x4<f32>,
+    inverse_projection: mat4x4<f32>,
+    inverse_view: mat4x4<f32>,
+    camera_position: vec3<f32>,
+    time: f32,
 };
 
 @group(0) @binding(0)
@@ -24,13 +24,13 @@ fn signed_distance_to_sphere(point: vec3<f32>, radius: f32) -> f32 {
 
 fn signed_distance_to_cube(point: vec3<f32>, size: f32) -> f32 {
     let q = abs(point) - size;
-    return length(max(q, vec3<f32>(0.0))) + min(max(q.x ,max(q.y, q.z)), 0.0);
+    return length(max(q, vec3<f32>(0.0))) + min(max(q.x, max(q.y, q.z)), 0.0);
 }
 
 struct PolarSphere {
-    q: f32;
-    f: f32;
-    t: f32;
+    q: f32,
+    f: f32,
+    t: f32,
 }
 
 fn to_polar_sphere(point: vec3<f32>) -> PolarSphere {
@@ -58,27 +58,27 @@ fn from_polar_sphere(sphere: PolarSphere) -> vec3<f32> {
 }
 
 fn signed_distance_to_mandelbulb(point: vec3<f32>, iterations: u32, power: f32) -> f32 {
-	var z = point;
-	var q = 0.0;
-	var dr = 1.0;
+    var z = point;
+    var q = 0.0;
+    var dr = 1.0;
 
     for (var i = 0u; i < iterations; i = i + 1u) {
         let polar = to_polar_sphere(z);
         q = polar.q;
 
-		if (q > 2.0) {
+        if q > 2.0 {
             break;
         }
-        
-		dr =  pow(q, power - 1.0)*power*dr + 1.0;
-		
+
+        dr = pow(q, power - 1.0) * power * dr + 1.0;
+
         let polar_pow = pow_polar_sphere(polar, power);
 
         let pow_cartesian = from_polar_sphere(polar_pow);
 
         z = pow_cartesian + point;
-	}
-	return 0.5*log(q)*q/dr;
+    }
+    return 0.5 * log(q) * q / dr;
 }
 
 fn map(point: vec3<f32>) -> f32 {
@@ -103,7 +103,7 @@ fn colored_map(point: vec3<f32>) -> vec2<f32> {
         d2,
         .2
     );
-    
+
     return vec2<f32>(m, clamp((d1 - d2) / .2, 0.0, 1.0));
 }
 
@@ -114,7 +114,7 @@ fn smooth_min(a: f32, b: f32, smoothing: f32) -> f32 {
 
 fn normal(point: vec3<f32>) -> vec3<f32> {
     let eps = 0.001;
-    
+
     let normal = vec3<f32>(
         map(point + vec3<f32>(eps, 0.0, 0.0)) - map(point + vec3<f32>(-eps, 0.0, 0.0)),
         map(point + vec3<f32>(0.0, eps, 0.0)) - map(point + vec3<f32>(0.0, -eps, 0.0)),
@@ -133,18 +133,20 @@ fn raymarch(position: vec3<f32>, ray: vec3<f32>) -> vec4<f32> {
     for (var i = 0u; i < max_steps; i = i + 1u) {
         let p = position + ray * t;
         // let d = map(p);
-        
+
         let m = colored_map(p);
         let d = m.x;
         let c = m.y;
 
-        if(d < threshold) {
+        if d < threshold {
             let lightDir = normalize(vec3<f32>(0.0, 1.0, -1.0));
-            
+
             var color = vec3<f32>(
-                1.0 - c, 0.0, c
+                1.0 - c,
+                0.0,
+                c
             );
-            
+
             let l = dot(normal(p), lightDir);
             // ret = vec4<f32>(vec3<f32>(c) - .2, 1.0);
             ret = vec4<f32>(l * color, 1.0);
@@ -161,7 +163,7 @@ fn raymarch(position: vec3<f32>, ray: vec3<f32>) -> vec4<f32> {
     
 // }
 
-@stage(vertex)
+@vertex
 fn vs_main(
     @builtin(vertex_index) in_vertex_index: u32,
 ) -> VertexOutput {
@@ -172,10 +174,10 @@ fn vs_main(
         vec2<f32>(1.0, 1.0),
     );
 
-    
+
     var out: VertexOutput;
     var pos = vec3<f32>(positions[in_vertex_index].xy, 0.0);
-    
+
     out.clip_position = vec4<f32>(pos, 1.0);
 
     out.position = pos;
@@ -184,18 +186,18 @@ fn vs_main(
 
     pos.z = 1.0;
     out.ray = (input_data.projection * input_data.inverse_projection * vec4<f32>(pos, 1.0)).xyz;
-    
+
     return out;
 }
 
 // Fragment shader
     
-@stage(fragment)
+@fragment
 fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
     let sphere_pos = vec3<f32>(0.0, 0.0, 2.0);
 
     var color = raymarch(-input_data.camera_position + vec3<f32>(0.0, 0.0, -1.0), normalize(in.ray));
-    
+
     let v = length(in.screen_coordinates / 5.0) / 1.0;
 
     color.x = color.x - v;
