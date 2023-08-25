8ddd6c9d423f101251861b59937f2bcf7e1c693c
Konrad Hapke <hap19520@spengergasse.at>
2023-08-25 14:30:47 +0200

    fixed some implementation changes of webgpu

--- src/App.svelte
+++ src/App.svelte
@@ -3,31 +3,33 @@
 	import shader from "./shaders/mandelbrot.wgsl";
 
 	let canvas: HTMLCanvasElement;
-	
+
 	async function init() {
 		function exit() {
-			console.error("No GPU adapter found! Try enabling the experimental flag \"#enable-unsafe-webgpu\" under \"chrome://flags\"");
+			console.error(
+				'No GPU adapter found! Try enabling the experimental flag "#enable-unsafe-webgpu" under "chrome://flags"'
+			);
 			return;
 		}
-		
+
 		if (!navigator.gpu) exit();
 		const adapter = await navigator.gpu.requestAdapter();
-		if(!adapter)
-			exit();
-  		const device = await adapter.requestDevice();
-		const context = canvas.getContext('webgpu');
+		if (!adapter) exit();
+		const device = await adapter.requestDevice();
+		const context = canvas.getContext("webgpu");
 
 		const pixelRatio = window.devicePixelRatio || 1;
 		const presentationSize = [
 			canvas.clientWidth * pixelRatio,
-			canvas.clientHeight * pixelRatio
+			canvas.clientHeight * pixelRatio,
 		];
-		const presentationFormat = context.getPreferredFormat(adapter);
+		// @ts-ignore
+		const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
 
 		context.configure({
 			device,
 			format: presentationFormat,
-			size: presentationSize
+			size: presentationSize,
 		});
 
 		const shaderModule = device.createShaderModule({
@@ -35,22 +37,24 @@
 		});
 
 		const pipeline = device.createRenderPipeline({
+			// @ts-ignore
+			layout: "auto",
 			vertex: {
 				module: shaderModule,
-				entryPoint: 'vs_main',
+				entryPoint: "vs_main",
 			},
 			fragment: {
 				module: shaderModule,
-				entryPoint: 'fs_main',
+				entryPoint: "fs_main",
 				targets: [
 					{
 						format: presentationFormat,
-					}
-				]
+					},
+				],
 			},
 			primitive: {
-				topology: 'triangle-strip',
-			}
+				topology: "triangle-strip",
+			},
 		});
 
 		class InputData {
@@ -70,7 +74,7 @@
 				this.g_mod = Math.random() * 3;
 			}
 		}
-		
+
 		const inputBufferSize = 6 * 4;
 		const inputBuffer = device.createBuffer({
 			size: inputBufferSize,
@@ -84,74 +88,80 @@
 					binding: 0,
 					resource: {
 						buffer: inputBuffer,
-					}
-				}
-			]
+					},
+				},
+			],
 		});
 
 		let inputData = new InputData();
-		
+
 		var pressedKeys: string[] = [];
 
 		function frame() {
-			if(!context) return;
+			if (!context) return;
 
 			document.addEventListener("keydown", (e) => {
-				if(!pressedKeys.includes(e.key))
-				pressedKeys.push(e.key.toLowerCase());
+				if (!pressedKeys.includes(e.key))
+					pressedKeys.push(e.key.toLowerCase());
 			});
 			document.addEventListener("keyup", (e) => {
 				pressedKeys.splice(pressedKeys.indexOf(e.key), 1);
 			});
 
-			const speed = .01;
+			const speed = 0.01;
 			const zoomSpeed = speed;
 			const moveSpeed = speed / Math.pow(Math.E, inputData.zoom);
 
-			if(pressedKeys.includes("w")) {
+			if (pressedKeys.includes("w")) {
 				inputData.y += moveSpeed;
 			}
-			if(pressedKeys.includes("s")) {
+			if (pressedKeys.includes("s")) {
 				inputData.y -= moveSpeed;
 			}
-			if(pressedKeys.includes("d")) {
+			if (pressedKeys.includes("d")) {
 				inputData.x += moveSpeed;
 			}
-			if(pressedKeys.includes("a")) {
+			if (pressedKeys.includes("a")) {
 				inputData.x -= moveSpeed;
 			}
-			if(pressedKeys.includes("+")) {
+			if (pressedKeys.includes("+")) {
 				inputData.zoom += zoomSpeed;
 			}
-			if(pressedKeys.includes("-")) {
+			if (pressedKeys.includes("-")) {
 				inputData.zoom -= zoomSpeed;
 			}
-			if(pressedKeys.includes("1")) {
+			if (pressedKeys.includes("1")) {
 				inputData.r_mod += zoomSpeed;
 			}
-			if(pressedKeys.includes("2")) {
+			if (pressedKeys.includes("2")) {
 				inputData.r_mod -= zoomSpeed;
 			}
-			if(pressedKeys.includes("3")) {
+			if (pressedKeys.includes("3")) {
 				inputData.g_mod += zoomSpeed;
 			}
-			if(pressedKeys.includes("4")) {
+			if (pressedKeys.includes("4")) {
 				inputData.g_mod -= zoomSpeed;
 			}
-			if(pressedKeys.includes("5")) {
+			if (pressedKeys.includes("5")) {
 				inputData.b_mod += zoomSpeed;
 			}
-			if(pressedKeys.includes("6")) {
+			if (pressedKeys.includes("6")) {
 				inputData.b_mod -= zoomSpeed;
 			}
-			if(pressedKeys.includes("r")) {
+			if (pressedKeys.includes("r")) {
 				inputData = new InputData();
 			}
-			
+
 			const commandEncoder = device.createCommandEncoder();
 
-			device.queue.writeBuffer(inputBuffer, 0, new Float32Array(Object.entries(inputData).map(([key, value]) => value)));
-			
+			device.queue.writeBuffer(
+				inputBuffer,
+				0,
+				new Float32Array(
+					Object.entries(inputData).map(([key, value]) => value)
+				)
+			);
+
 			const textureView = context.getCurrentTexture().createView();
 
 			const renderPassDescriptor: GPURenderPassDescriptor = {
@@ -159,13 +169,14 @@
 					{
 						view: textureView,
 						clearValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
-						loadOp: 'clear',
-						storeOp: 'store',
+						loadOp: "clear",
+						storeOp: "store",
 					},
 				],
 			};
 
-			const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+			const passEncoder =
+				commandEncoder.beginRenderPass(renderPassDescriptor);
 			passEncoder.setPipeline(pipeline);
 			passEncoder.setBindGroup(0, inputBindGroup);
 			passEncoder.draw(4);
@@ -178,8 +189,6 @@
 		requestAnimationFrame(frame);
 	}
 
-	
-
 	onMount(() => {
 		init();
 	});
@@ -187,7 +196,7 @@
 	init();
 </script>
 
-<canvas bind:this="{canvas}"></canvas>
+<canvas bind:this={canvas} />
 
 <style>
 	canvas {
--- src/shaders/mandelbrot.wgsl
+++ src/shaders/mandelbrot.wgsl
@@ -1,24 +1,24 @@
 // Vertex shader
 
 struct VertexOutput {
-    @builtin(position) clip_position: vec4<f32>;
-    @location(0) position: vec2<f32>;
+    @builtin(position) clip_position: vec4<f32>,
+    @location(0) position: vec2<f32>,
 };
 
 struct InputData {
-    zoom: f32;
-    x: f32;
-    y: f32;
-    r_mod: f32;
-    g_mod: f32;
-    b_mod: f32;
+    zoom: f32,
+    x: f32,
+    y: f32,
+    r_mod: f32,
+    g_mod: f32,
+    b_mod: f32,
 };
 
 @group(0) @binding(0)
 var<uniform> input_data: InputData;
 
 
-@stage(vertex)
+@vertex
 fn vs_main(
     @builtin(vertex_index) in_vertex_index: u32,
 ) -> VertexOutput {
@@ -28,14 +28,14 @@ fn vs_main(
         vec2<f32>(-1.0, 1.0),
         vec2<f32>(1.0, 1.0),
     );
-    
+
     let e = 2.7182;
     var out: VertexOutput;
     let x = f32(1 - i32(in_vertex_index)) * 0.05;
     let y = f32(i32(in_vertex_index & 1u) * 2 - 1) * 0.05;
     out.clip_position = vec4<f32>(positions[in_vertex_index].xy, 0.0, 1.0);
     out.position = vec2<f32>(positions[in_vertex_index].x * 2.0 - 0.5, positions[in_vertex_index].y);
-    out.position = (out.position) * pow(e, -input_data.zoom)  + vec2<f32>(input_data.x, input_data.y);
+    out.position = (out.position) * pow(e, -input_data.zoom) + vec2<f32>(input_data.x, input_data.y);
     return out;
 }
 
@@ -47,20 +47,19 @@ fn square_complex(c: vec2<f32>) -> vec2<f32> {
     return vec2<f32>(x, y);
 }
 
-@stage(fragment)
+@fragment
 fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
     let iterations = 1000;
 
     var z = vec2<f32>(0.0);
     let c = in.position;
 
-    for(var i = 0; i < iterations; i = i + 1) {
+    for (var i = 0; i < iterations; i = i + 1) {
         z = square_complex(z) + c;
-        if(z.x * z.x + z.y * z.y > 4.0) {
+        if z.x * z.x + z.y * z.y > 4.0 {
             let f = f32(i) / 10.0;
             return vec4<f32>(f % input_data.r_mod, f % input_data.g_mod, f % input_data.b_mod, 1.0);
         }
-        
     }
 
     return vec4<f32>(0.0, 0.0, 0.0, 1.0);
